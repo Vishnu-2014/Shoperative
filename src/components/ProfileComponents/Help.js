c30b937e9bf3bcabca13c691b42ec7bad3a99718
Vishnu-2014 <vishnureddy2014@gmail.com>
2023-06-07 16:30:22 +0530

    Help Screen Validations Updated

--- src/components/ProfileComponents/Help.js
+++ src/components/ProfileComponents/Help.js
@@ -1,21 +1,72 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet, TextInput, Pressable} from 'react-native';
 import {HeaderComponent} from '../CustomComponents/HeaderComponent';
+import {Snackbar} from 'react-native-paper';
 
 const Help = () => {
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+  const [err, setErr] = useState('');
+  const [visible, setVisible] = useState(false);
+
+  const onDismiss = () => {
+    setVisible(false);
+  };
+
+  const snackBar = () => {
+    return (
+      <Snackbar
+        visible={visible}
+        onDismiss={onDismiss}
+        action={{label: 'Close'}}>
+        {err}
+      </Snackbar>
+    );
+  };
+
+  const handleSubjectInput = data => {
+    setSubject(data);
+  };
+  const handleMessageInput = data => {
+    setMessage(data);
+  };
+
+  const HelpValidation = () => {
+    if (subject === '') {
+      setVisible(true);
+      setErr('Please Enter Subject');
+    } else if (message === '') {
+      setVisible(true);
+      setErr('Please Enter Message');
+    } else {
+      setVisible(true);
+      setErr('Request Sent');
+    }
+  };
+
   return (
     <>
       <HeaderComponent title={'Help'} />
       <View style={styles.container}>
         <View style={styles.contentContainerStyles}>
           <Text style={styles.textFeildStyles}>Subject</Text>
-          <TextInput style={styles.inputFeilds1} />
+          <TextInput
+            style={styles.inputFeilds1}
+            onChangeText={handleSubjectInput}
+          />
           <Text style={styles.textFeildStyles}>Message</Text>
-          <TextInput style={styles.inputFeilds2} multiline={true} />
-          <Pressable style={styles.buttonStyles}>
+          <TextInput
+            style={styles.inputFeilds2}
+            multiline={true}
+            onChangeText={handleMessageInput}
+          />
+          <Pressable
+            style={styles.buttonStyles}
+            onPress={() => HelpValidation()}>
             <Text style={styles.buttonTextStyles}>REQUEST SUPPORT</Text>
           </Pressable>
         </View>
+        {snackBar()}
       </View>
     </>
   );
